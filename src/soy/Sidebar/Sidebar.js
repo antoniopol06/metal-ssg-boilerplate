e80f8808b3e8d902f2ac2457301edaf6b9425c66
Zeno Rocha <zno.rocha@gmail.com>
2016-11-23 15:25:51 -0800

    Includes toggler inside sidebar

--- src/soy/Sidebar/Sidebar.js
+++ src/soy/Sidebar/Sidebar.js
@@ -2,12 +2,18 @@
 
 import Component from 'metal-component';
 import Soy from 'metal-soy';
+import Toggler from 'metal-toggler';
 
 import templates from './Sidebar.soy';
 
 class Sidebar extends Component {
 	attached() {
 		console.log('Sidebar attached.');
+
+		new Toggler({
+			content: '.sidebar-list-1',
+			header: '.sidebar-header'
+		});
 	}
 };
 
